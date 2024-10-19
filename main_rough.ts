3b1d46029dccc611eb91932cd7579b1e19912d97
GracyBeula <gracybeula024@gmail.com>
2024-10-19 16:35:42 +0530

    push

--- main_rough.ts
+++ main_rough.ts
@@ -3,4 +3,5 @@ let message: string = "Hello, TypeScript!";
 let message1: string = "gracy beula";
 let message2: string = "gracy beula gracy";
 let message3: string = "gracy beula gracy beula";
+let message4: string = "push"
 console.log(message);
