ccda7446194e812f68e79c6519bcc53011a9b45e
GracyBeula <gracybeula024@gmail.com>
2024-10-19 16:32:39 +0530

    save two

--- main_rough.ts
+++ main_rough.ts
@@ -2,4 +2,5 @@
 let message: string = "Hello, TypeScript!";
 let message1: string = "gracy beula";
 let message2: string = "gracy beula gracy";
+let message3: string = "gracy beula gracy beula";
 console.log(message);
